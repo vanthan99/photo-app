ab9c409dea4eae8353024ead31bc5a83f425c7a3
vanthan99 <vanthan.ad.it@gmail.com>
2020-08-28 21:52:51 +0700

    Updates

---
+++ precache-manifest.45fca8bbcb403500c3b531c4111bfabb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b57cdbeb42088776c8bc3ab4dd99f3d",
+    "url": "/photo-app/index.html"
+  },
+  {
+    "revision": "7a82112f1f384c78b359",
+    "url": "/photo-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1ae9459b1739d53bec8f",
+    "url": "/photo-app/static/css/4.44f0f616.chunk.css"
+  },
+  {
+    "revision": "8b4a82075f0e86475be6",
+    "url": "/photo-app/static/css/main.ede3027c.chunk.css"
+  },
+  {
+    "revision": "7a82112f1f384c78b359",
+    "url": "/photo-app/static/js/2.1370ba5d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/photo-app/static/js/2.1370ba5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2514f095de393d1934e",
+    "url": "/photo-app/static/js/3.06c556b5.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/photo-app/static/js/3.06c556b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae9459b1739d53bec8f",
+    "url": "/photo-app/static/js/4.03d25a65.chunk.js"
+  },
+  {
+    "revision": "8b4a82075f0e86475be6",
+    "url": "/photo-app/static/js/main.8558f5b9.chunk.js"
+  },
+  {
+    "revision": "4badf3c3a51203325cd0",
+    "url": "/photo-app/static/js/runtime-main.16b22f19.js"
+  },
+  {
+    "revision": "146c36c8ff0d0879ac2151a8c5d01dbc",
+    "url": "/photo-app/static/media/blue-bg.146c36c8.jpg"
+  },
+  {
+    "revision": "cb9ae265b9fad5e6f9d382d1344b3269",
+    "url": "/photo-app/static/media/colorful-bg.cb9ae265.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e6624e7ee3d44d0011cd9c032d126b8d",
+    "url": "/photo-app/static/media/orange-bg.e6624e7e.jpg"
+  },
+  {
+    "revision": "c8426a377ada4d0624a76711766ab918",
+    "url": "/photo-app/static/media/pink-bg.c8426a37.jpg"
+  }
+]);
